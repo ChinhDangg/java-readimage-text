68ae7572ced9538f5a6e140c67ed553e751cba1a
ChinhDangg <61719308+ChinhDangg@users.noreply.github.com>
2021-12-15 10:21:10 +0700

    Create ReadText.java
    main class that start the reading

---
+++ ReadText.java
@@ -0,0 +1,6 @@
+public class ReadText {
+  public static void main(String[] args) {
+    String image_directory = "C:\\Name\\Something\\Desktop"; //replace directory name here
+    System.out.println(ImageReader.extractImage(image_directory)); //result from reading image
+  }
+}
